f821d129d5c1a795baaac2e00864c6060e15a8db
lilliana0843 <98156747+lilliana0843@users.noreply.github.com>
2025-11-28 22:03:41 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,14 +3,13 @@ const lightboxImg = document.getElementById("lightbox-img");
 const thumbnails = document.querySelectorAll(".gallery img");
 
 thumbnails.forEach(img => {
-      img.addEventListener("click", () => {
-        lightboxImg.src = img.src;
-        lightbox.style.display = "flex";
-      });
-    });
+  img.addEventListener("click", () => {
+    lightboxImg.src = img.src;
+    lightbox.style.display = "flex";
+  });
+});
 
-    // 點擊遮罩或圖片關閉
-    lightbox.addEventListener("click", () => {
-      lightbox.style.display = "none";
-    });
- 
+// 點擊任意處關閉
+lightbox.addEventListener("click", () => {
+  lightbox.style.display = "none";
+});
